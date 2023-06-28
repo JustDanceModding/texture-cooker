918741ca2a1c140fce2cbbaecb4636d29ed683f6
sen_ <88703078+silencer1809@users.noreply.github.com>
2023-06-28 11:20:24 +0200

    NX Code's Modified
    Added support for python and compiled version of xtx_extract.

--- src/CookerFunctions/ConvertPlatform.py
+++ src/CookerFunctions/ConvertPlatform.py
@@ -12,7 +12,25 @@ class ConvertTexture:
             system(f"{binPath}\\dds2gtf.exe \"{outImage}\" -o \"{outImage}\"")
 
         if platform == "nx":
-            system(f"{binPath}\\xtx_extract.exe -o \"{outImage}\" \"{image}\"")
+            try:
+                # Mechanism to detect if theres exe version or python one.
+                xtx_ext = open(f"{binPath}\\xtx_extract.py", "rb")
+                xtx_ext = "xtx_extract.py"
+            except FileNotFoundError:
+                pass
+            # Two try and except are needed for avoiding errors on the except.
+            try:
+                # Mechanism to detect if theres exe verion of python one.
+                xtx_ext = open(f"{binPath}\\xtx_extract.exe", "rb")
+                xtx_ext = "xtx_extract.exe"
+            except FileNotFoundError:
+                pass
+            
+            if xtx_ext.endswith(".py"):
+                arg = "py "
+            else:
+                arg = ""
+            system(f"{arg}{binPath}\\{xtx_ext} -o \"{outImage}\" \"{image}\"")
 
         if platform == "x360":
             system(f"{binPath}\\Bundler.exe \"{image}\"")
